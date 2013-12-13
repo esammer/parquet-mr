2b0009d977699394524818c19034b80a099564df
Tianshuo Deng <tdeng@twitter.com>
2013-12-13 14:49:27 -0800

    more docs

--- parquet-thrift/src/main/java/parquet/thrift/BufferedProtocolReadToWrite.java
+++ parquet-thrift/src/main/java/parquet/thrift/BufferedProtocolReadToWrite.java
@@ -425,20 +425,24 @@ public class BufferedProtocolReadToWrite implements ProtocolPipe {
    */
   public static interface ReadWriteErrorHandler {
     /**
-     * handle when a record can not be read due to an exception
+     * handle when a record can not be read due to an exception,
+     * in this case the record will be skipped and this method will be called with the exception that caused reading failure
      *
      * @param e
      */
     void handleSkippedCorruptedRecord(SkippableException e);
 
     /**
-     * handle when a record that contains fields that are ignored, meaning that the schema provided does not cover all the columns in data
+     * handle when a record that contains fields that are ignored, meaning that the schema provided does not cover all the columns in data,
+     * the record will still be written but with fields that are not defined in the schema ignored.
+     * For each record, this method will be called at most once.
      */
     void handleRecordHasFieldIgnored();
 
     /**
-     * handle when a field gets ignored
-     *
+     * handle when a field gets ignored,
+     * notice the difference between this method and {@link #handleRecordHasFieldIgnored()} is that:
+     * for one record, this method maybe called many times when there are multiple fields not defined in the schema.
      * @param field
      */
     void handleFieldIgnored(TField field);
